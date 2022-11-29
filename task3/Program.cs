15728e2694f77680718f48c8bde1f08ade740894
Mashhroma <mashh@mail.ru>
2022-11-29 19:44:43 +0300

    task3 is solved

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4" 
+// 2 -> " -2, -1, 0, 1, 2"
+
+Console.WriteLine("Пожалуйста, напишите целое положительное число");
+
+int Number = int.Parse(Console.ReadLine()!);
+
+
+if (Number>0)
+{
+    int index = -Number;
+    while (index <= Number)
+    {
+        Console.Write($"{index} ");
+        index++;
+    }
+}
+else
+{
+    int index = Number;
+    while (index <= -Number)
+    {
+        Console.Write($"{index} ");
+        index++;
+    }
+}
