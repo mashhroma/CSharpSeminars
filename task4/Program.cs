2d14e061196d65b07a399f1213492d81f34cc747
Mashhroma <mashh@mail.ru>
2022-11-29 19:55:18 +0300

    task4 is solved

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6
+// 	782 -> 2
+// 	918 -> 8
+
+Console.Write("Введите, пожалуйста, трехзначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int restNumber = number % 10;
+
+if ((number>-1000 && number < -99) || ((number>99 && number < 1000)))
+{
+    Console.WriteLine($"Последняя цифра числа: {restNumber}.");
+}
+else
+{
+    Console.WriteLine($"Введенное число {number} не является трехзначным.");
+}
