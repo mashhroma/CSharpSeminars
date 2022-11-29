5c88b86b808bead8adc4488b2b9d8dba5bfee126
Mashhroma <mashh@mail.ru>
2022-11-29 19:23:39 +0300

    task2 is solved

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,4 +2,39 @@
 // 3 -> Среда 
 // 5 -> Пятница
 
-Console.WriteLine("Hello, World!");
+Console.Write("Пожалуйста, напишите цифру дня недели: ");
+string DateNum = Console.ReadLine()!;
+
+if (DateNum == "1")
+{
+    Console.WriteLine("Это - понедельник!");
+}
+
+else if (DateNum == "2")
+{
+    Console.WriteLine("Это - вторник!");
+}
+else if (DateNum == "3")
+{
+    Console.WriteLine("Это - среда!");
+}
+else if (DateNum == "4")
+{
+    Console.WriteLine("Это - четверг!");
+}
+else if (DateNum == "5")
+{
+    Console.WriteLine("Это - пятница!");
+}
+else if (DateNum == "6")
+{
+    Console.WriteLine("Это - суббота!");
+}
+else if (DateNum == "7")
+{
+    Console.WriteLine("Это - воскресенье!");
+}
+else
+{
+    Console.WriteLine("Это - не день недели!");
+}
