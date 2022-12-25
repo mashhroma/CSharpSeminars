3720e574b8e794824adadea2978663b10f8f42f3
Mashhroma <mashh@mail.ru>
2022-12-25 12:47:52 +0300

    seminar 8

---
+++ task65/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до M.
+
+string Numbers(int m, int n)
+{
+    if (n == m) return $"{n}";
+    if (n < m) return $"{n} {Numbers(m, n + 1)}";
+    else return $"{Numbers(m, n - 1)} {n}";
+}
+
+Console.Write(Numbers(1, 10));
